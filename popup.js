16790bda541061c092e2eb1af95e1d6c646152c7
Diogo Gomes <diogogomes@microsoft.com>
2023-01-05 20:07:17 +0000

    added auto notes

--- popup.js
+++ popup.js
@@ -1,5 +1,16 @@
 let letsGo = document.getElementById("letsGo");
 let browseApp = document.getElementById("browseApp");
+let copyNote = document.getElementById("copyNote");
+
+copyNote.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  copyNote.style.background = "green"
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setNote
+  });
+});
 
 browseApp.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -39,11 +50,19 @@ letsGo.addEventListener("click", async () => {
   });
 });
 
+
 function getInfoFromDFM(selection) {
-  statement = document.getElementById("id-c769b728-0b08-42b7-8c6c-19e1744381d6-1-msdfm_customerstatemente0dece4b-6fc8-4a8f-a065-082708572369-msdfm_customerstatement.fieldControl-text-box-text").value;
-  
-  appName = statement.toString().split("Microsoft.Web/sites/")[1].split(/\r?\n/)[0];
-  console.log("App name: " + appName);
+  var appName = ""
+  var statement = ""
+  try {
+    statement = document.getElementById("id-c769b728-0b08-42b7-8c6c-19e1744381d6-1-msdfm_customerstatemente0dece4b-6fc8-4a8f-a065-082708572369-msdfm_customerstatement.fieldControl-text-box-text").value;
+    appName = statement.toString().split("Microsoft.Web/sites/")[1].split(/\r?\n/)[0];
+    console.log("App name: " + appName);
+  } catch (error) {
+    console.log(error)
+    alert("Can't find the app name in the case as part of Microsoft.Web/sites - please open AppLens manually")
+    return
+  }
 
   timeframe = statement.toString().split("ProblemStartTime: ")[1].split(/\r?\n/)[0];
   day = timeframe.split(" ")[0].split("/");
@@ -108,12 +127,100 @@ function getInfoFromDFM(selection) {
     return parsedDate;
   }
 
+  return false
 }
-
 
 function goToSite() {
   statement = document.getElementById("id-c769b728-0b08-42b7-8c6c-19e1744381d6-1-msdfm_customerstatemente0dece4b-6fc8-4a8f-a065-082708572369-msdfm_customerstatement.fieldControl-text-box-text").value;
   appName = statement.toString().split("Microsoft.Web/sites/")[1].split(/\r?\n/)[0];
   url = "https://" + appName + ".azurewebsites.net";
   window.open(url, '_blank').focus();
+}
+
+function setNote(){
+  var div = document.createElement('div');
+  div.innerHTML = `
+  <div id=note style="display:none; font-family:Calibri">
+    <b><u>General Information</u></b>
+    <ul>
+      <li><b>App name: </b><span id="app_name"> N/A </span></li>
+      <li><b>Applens: </b><a id="applens" href="https://applens.trafficmanager.net/sites">General Information</a></li>
+      <li><b>Issue date/time (UTC): </b><span id="date"> N/A </span></li>
+      <li><b>Location: </b><span id="location"> N/A </span></li>
+    </ul>
+    <b>_____________________________________________</b>
+  </div>`;
+  document.body.appendChild(div)
+
+  
+  var appName = ""
+  var statement = ""
+  var timeframe = ""
+  var location = ""
+  var applens = ""
+  try {
+    statement = document.getElementById("id-c769b728-0b08-42b7-8c6c-19e1744381d6-1-msdfm_customerstatemente0dece4b-6fc8-4a8f-a065-082708572369-msdfm_customerstatement.fieldControl-text-box-text").value;
+    appName = statement.toString().split("Microsoft.Web/sites/")[1].split(/\r?\n/)[0];
+    timeframe = statement.toString().split("ProblemStartTime: ")[1].split(/\r?\n/)[0];
+    location = statement.toString().split("Location: ")[1].split(/\r?\n/)[0];
+
+    caseBox = document.querySelector('[id*="headerControlsList_"]').innerHTML;
+    caseNumber = caseBox.toString().split(" |")[0].split(">").pop();
+    day = timeframe.split(" ")[0].split("/");
+    hours = timeframe.split(" ")[1].toString().slice(0, -3);
+    issueDate = day[2] + "-" + day[0] + "-" + day[1] + "%20" + hours;
+    endTime = getCurrentTime();
+    endTime = addSmallInitialBreak(endTime);
+    startTime = getDate(endTime, 24) 
+    applens = "https://applens.trafficmanager.net/sites/" + appName + "?startTime=" + startTime + "&endTime=" + endTime + "&caseNumber=" + caseNumber;
+
+    document.getElementById('app_name').innerHTML = appName;
+    document.getElementById('applens').href = applens;
+    document.getElementById('date').innerHTML = timeframe;
+    document.getElementById('location').innerHTML = location;
+
+    function getDate(startTime, difference) {
+      initDate = startTime.replace("%20", " ")
+      var dt = new Date(initDate);
+      dt.setHours( dt.getHours() - difference);
+      end = dt.toLocaleString("en-GB");
+      endDate = end.split(", ")[0].split("/")
+      endHour = end.split(", ")[1].toString().slice(0, -3); 
+      parsedDate = endDate[2] + "-" + endDate[1] + "-" + endDate[0] + "%20" + endHour;
+      return parsedDate;
+    }
+    
+    function getCurrentTime() {
+      const isoStr = new Date().toISOString();
+      today = isoStr.replace("T", "%20").slice(0, -8);
+      console.log("Today in UTC: " + today);
+      return today;
+    }
+
+    function addSmallInitialBreak(startTime) {
+      initDate = startTime.replace("%20", " ")
+      var dt = new Date(initDate);
+      dt.setMinutes( dt.getMinutes() - 16);
+      end = dt.toLocaleString("en-GB");
+      endDate = end.split(", ")[0].split("/")
+      endHour = end.split(", ")[1].toString().slice(0, -3); 
+      parsedDate = endDate[2] + "-" + endDate[1] + "-" + endDate[0] + "%20" + endHour;
+      return parsedDate;
+    }
+
+  } catch (error) {
+    console.log(error)
+    alert("Can't find the app name in the case as part of Microsoft.Web/sites - default note was copied")
+  }
+  
+  str = document.getElementById('note')
+  console.log("print str")
+  console.log(str)
+  str.style.display = "inline"
+  var range = document.createRange()
+  range.selectNode(str)
+  window.getSelection().addRange(range)
+  document.execCommand('copy')
+  str.style.display = "none"
+  
 }
